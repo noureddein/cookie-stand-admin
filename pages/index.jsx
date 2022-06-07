54a45d53de4d48c1a33d41f916a7f1d7d3f17d90
noureddein <noureddein@gmail.com>
2022-06-07 15:35:50 +0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Cookie Stand Admin</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className=''></header>
+
+      <main className="">
+        
+      </main>
+
+      <footer className="">
+      </footer>
+    </div>
+  )
+}
+
+export default Home
